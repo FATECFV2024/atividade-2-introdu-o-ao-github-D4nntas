909a913fc254eb76029bce6026c7f06c38152e3d
D4nntas <143447163+D4nntas@users.noreply.github.com>
2024-09-10 23:37:44 -0300

    Update App.java

--- FazerVsCode/src/App.java
+++ FazerVsCode/src/App.java
@@ -2,7 +2,7 @@
         public static void main(String[] args) throws Exception {
             // Cria uma instância (objeto) da classe RGM2920482411026, chamada InfoAluno
             RGM2920482411026 InfoAluno = new RGM2920482411026();
-            // saida com todos os dados
+            // saida com todos os dados informados
             System.out.println(String.format("Nome Completo: %s\n RGM: %s\n Idade: %s\n GitHub: %s\n LinkedIn: %s\n Curso e Turma: %s", InfoAluno.nomeCompleto, InfoAluno.RGM, InfoAluno.idade, InfoAluno.github, InfoAluno.linkedin, InfoAluno.CursoTurma));
 
 
